2dcd205254f2661f63b056e02c960ee91ad1cb59
jiang <jialei.jiang@outlook.com>
2019-07-11 20:44:26 +0800

    新项目开心消消乐各种布局学习

---
+++ demo/src/main/java/com/liniou/demo/RabbitView.java
@@ -0,0 +1,65 @@
+package com.liniou.demo;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class RabbitView extends View {
+
+    private float bitmapX;
+    private float bitmapY;
+    private Paint paint;
+
+    //!构造函数
+    public RabbitView(Context context) { //初始化组件
+        super(context);
+        bitmapX = 540;
+        bitmapY = 960;
+    }
+
+    //!绘图
+    @Override
+    protected void onDraw(Canvas canvas) {
+        paint = new Paint();
+        super.onDraw(canvas);
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.mipmap.yi); //位图
+        canvas.drawBitmap(bitmap, bitmapX, bitmapY, paint);
+        if(bitmap.isRecycled()){ //强制回收图片
+            bitmap.recycle();
+        }
+    }
+
+    public void onTouchLinstener(){
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                bitmapX = motionEvent.getX();
+                bitmapY = motionEvent.getY();
+                invalidate();
+                return true;
+            }
+        });
+    }
+
+
+    public float getBitmapX() {
+        return bitmapX;
+    }
+
+    public void setBitmapX(float bitmapX) {
+        this.bitmapX = bitmapX;
+    }
+
+    public float getBitmapY() {
+        return bitmapY;
+    }
+
+    public void setBitmapY(float bitmapY) {
+        this.bitmapY = bitmapY;
+    }
+
+}
